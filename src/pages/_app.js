6b796af0bf8a1c8f4b7312455f2a37e6d393f6d5
faruqismael <farusmith5@gmail.com>
2023-10-10 11:56:05 +0300

    loader to the new _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,15 +7,49 @@ import "react-toastify/dist/ReactToastify.css";
 
 import "../styles/scss/style.scss";
 import "../styles/globals.css";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { ToastContainer } from "react-toastify";
+import CookieConsent from "../components/sections/CookieConsent";
+import Loader from "../components/Loader";
 
-export default class MyApp extends App {
-  render() {
-    const { Component, pageProps } = this.props;
-    return (
-      <>
-        <DefaultSeo {...SEO} />
-        <Component {...pageProps} />
-      </>
-    );
-  }
+export default function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(false);
+    const handleRouteChange = (url) => {
+      setLoading(true);
+    };
+
+    const handleRouteChangeComplete = () => {
+      setLoading(false);
+    };
+
+    router.events.on("routeChangeStart", handleRouteChange);
+    router.events.on("routeChangeComplete", handleRouteChangeComplete);
+
+    return () => {
+      router.events.off("routeChangeStart", handleRouteChange);
+      router.events.off("routeChangeComplete", handleRouteChangeComplete);
+    };
+  }, [router.events]);
+
+  return (
+    <>
+      {!loading ? (
+        <>
+          <ToastContainer />
+          <CookieConsent />
+          <Component {...pageProps} />
+        </>
+      ) : (
+        <>
+          <DefaultSeo {...SEO} />
+          <Loader />
+        </>
+      )}
+    </>
+  );
 }
