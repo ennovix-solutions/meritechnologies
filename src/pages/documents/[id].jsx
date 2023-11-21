307958c1805c8d570de9dcb07363d4dfe80fa9a2
faruqismael <farusmith5@gmail.com>
2023-11-21 13:45:58 +0300

    adsfasd

--- src/pages/documents/[id].jsx
+++ src/pages/documents/[id].jsx
@@ -114,8 +114,8 @@ function Document() {
       {fullPage ? (
         <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
           <Viewer
-            fileUrl={`http://files.meritechnologies.com/files/web-pricing.pdf`}
-            // fileUrl={`/docs/${router.query.id}.pdf`}
+            // fileUrl={`http://files.meritechnologies.com/files/web-pricing.pdf`}
+            fileUrl={`/docs/${router.query.id}.pdf`}
             plugins={[defaultLayoutPluginInstance]}
           />
         </Worker>
@@ -127,9 +127,9 @@ function Document() {
             </h4>
             <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
               <Viewer
-                // fileUrl={`/docs/${router.query.id}.pdf`}
+                fileUrl={`/docs/${router.query.id}.pdf`}
 
-                fileUrl={`http://files.meritechnologies.com/files/${router.query.id}.pdf`}
+                // fileUrl={`http://files.meritechnologies.com/files/${router.query.id}.pdf`}
                 plugins={[defaultLayoutPluginInstance]}
               />
             </Worker>
